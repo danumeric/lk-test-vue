cb8ee02fffe15963581ee9114cacb026c6b97971
danumeric <89909851+danumeric@users.noreply.github.com>
2022-08-02 14:49:26 +0300

    server configs

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PersonalAccountView from '../views/PersonalAccountView.vue'
 import AuthView from '../views/AuthView.vue'
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory()(process.env.BASE_URL),
   routes
 })
 
